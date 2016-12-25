4163aa239c0dd9f9528317cbcbe26132e7c58eb8
Yuttana <yuttana76>
2016-12-25 22:39:17 +0700

    Initial project FullStack

---
+++ src/main/java/com/fullstact/Fullstact2Application.java
@@ -0,0 +1,12 @@
+package com.fullstact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Fullstact2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Fullstact2Application.class, args);
+	}
+}
---
+++ src/main/java/com/fullstact/controller/HelloWorldController.java
@@ -0,0 +1,13 @@
+package com.fullstact.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloWorldController {
+
+	@RequestMapping("/")
+	public String sayHello(){
+		return "index";
+	}
+}
